10e6b596333496291cd8dcb493ca6ece50bc6748
Karan-Developer-69 <homepc8890@gmail.com>
2026-03-28 09:28:48 +0530

    UI fixed

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins:["10.38.214.34"],
   serverExternalPackages: ['@xenova/transformers', 'onnxruntime-node', 'sharp'],
 };
 
